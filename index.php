0baebad8884e07961799c80a0ba981d9fc5786b2
James Murray <thejamesm@users.noreply.github.com>
2018-01-14 01:14:38 +0000

    Initial commit
    Static content, upside-down

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+        <meta name="google" value="notranslate">
+        <title>sʇɔǝɹıpǝɹ qɯo𐐒 ʇuɐı⅁</title>
+        <link rel="stylesheet" href="style.css">
+        <script>
+            window.onload = function() {
+                window.scrollTo( 0, document.body.scrollHeight );
+            };
+        </script>
+    </head>
+    <body>
+        <section>
+            <h1>Giant Bomb</h1>
+            <a href="http://abbyzboyz.com">abbyzboyz.com</a><br>
+            <a href="http://abbyfrombrooklyn.biz">abbyfrombrooklyn.biz</a><br>
+            <a href="http://anothervideogamewebsite.com">anothervideogamewebsite.com</a><br>
+            <a href="http://awasteofbandwidth.com">awasteofbandwidth.com</a><br>
+            <a href="http://bombagigante.com">bombagigante.com</a><br>
+            <a href="http://cornfights.com">cornfights.com</a><br>
+            <a href="http://dogbottom.com">dogbottom.com</a><br>
+            <a href="http://dontcallnorm.com">dontcallnorm.com</a><br>
+            <a href="http://fightingame.science">fightingame.science</a><br>
+            <a href="http://fightinggame.science">fightinggame.science</a><br>
+            <a href="http://fuckdanryckert.com">fuckdanryckert.com</a><br>
+            <a href="http://giantasshole.com">giantasshole.com</a><br>
+            <a href="http://giantboat.com">giantboat.com</a><br>
+            <a href="http://gooddestinystuff.biz">gooddestinystuff.biz</a><br>
+            <a href="http://guyswhoplayawholelotofvideo.games">guyswhoplayawholelotofvideo.games</a><br>
+            <a href="http://hotsauceparty.biz">hotsauceparty.biz</a><br>
+            <a href="http://murderis.land">murderis.land</a><br>
+            <a href="http://nuke.com">nuke.com</a><br>
+            <a href="http://rivalvideogamewebsite.com">rivalvideogamewebsite.com</a><br>
+            <a href="http://snakesnakesnake.com">snakesnakesnake.com</a><br>
+            <a href="http://spookyslide.com">spookyslide.com</a><br>
+            <a href="http://superobjectivevideogame.review">superobjectivevideogame.review</a><br>
+            <a href="http://themostvideogame.review">themostvideogame.review</a><br>
+            <a href="http://thenumbertwopodcastintheworld.com">thenumbertwopodcastintheworld.com</a><br>
+            <a href="http://videogames.com">videogames.com</a><br>
+        </section>
+        <section>
+            <h1>Misc</h1>
+            <a href="http://batmanbatmanbatman.info">batmanbatmanbatman.info</a><br>
+            <a href="http://cocainemountain.com">cocainemountain.com</a><br>
+            <a href="http://danryckert.eu">danryckert.eu</a><br>
+            <a href="http://gamebomb.ru">gamebomb.ru</a><br>
+            <a href="http://giantabby.com">giantabby.com</a><br>
+            <a href="http://giantblooper.com">giantblooper.com</a><br>
+            <a href="http://giantbuilding.com">giantbuilding.com</a><br>
+            <a href="http://mariopartyparty.com">mariopartyparty.com</a><br>
+        </section>
+    </body>
+</html>
